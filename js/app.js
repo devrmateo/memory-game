219a2cab80345264351af7f117c490a3de79f334
Matthew Smith <mateoelcule@gmail.com>
2018-07-26 10:02:16 -0700

    Set up event listener for card and created openCard function

--- js/app.js
+++ js/app.js
@@ -29,7 +29,18 @@ function shuffle(array) {
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ */
+ let deck = document.querySelector('.deck');
+ deck.addEventListener('click', toggleCard);
+
+ function toggleCard(e) {
+  e.target.classList.add('open');
+  e.target.classList.add('show');
+ }
+
+ /*
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)*/
+ /*
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
